206767399ced514e3564a9141cf38b886d512aab
Jane Crafty <craffty@gmail.com>
2024-12-09 12:04:02 +0200

    parsed and printed 2nd message

--- json_parcing.py
+++ json_parcing.py
@@ -1,9 +1,7 @@
 import json
-from json.decoder import JSONDecodeError
 
 json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
-
 second_message = obj['messages'][1]['message']
 
 print(second_message)
