efa74ed18237c23097cbb814dfc69870da22d982
Jane Crafty <craffty@gmail.com>
2024-12-18 11:22:32 +0200

    add ex16 get user test

--- lib/assertions.py
+++ lib/assertions.py
@@ -25,3 +25,22 @@ class Assertions:
             assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON doesn`t have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_no_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not a JSON. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON shouldn`t have key '{name}. But it`s present"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not a JSON. Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn`t have key '{name}'"
